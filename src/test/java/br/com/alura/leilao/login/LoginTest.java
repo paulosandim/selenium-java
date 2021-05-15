f9609e418d871d8217c45fd45b3a60266cdb23e5
paulo sandim <pauloedusandim@gmail.com>
2021-05-15 18:33:16 -0300

    teste do formulario de login

---
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -0,0 +1,25 @@
+package br.com.alura.leilao.login;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+	
+	@Test
+	public void loginComDadosValidos() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver browser = new ChromeDriver();
+		browser.navigate().to("http://localhost:8080/login");
+		browser.findElement(By.id("username")).sendKeys("fulano");
+		browser.findElement(By.id("password")).sendKeys("pass");
+		browser.findElement(By.id("login-form")).submit();
+		
+		Assert.assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
+		Assert.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
+		browser.quit();
+	}
+
+}
